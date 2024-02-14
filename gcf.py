7f7d6a9d12f444fcfa8a1aa1a8a9105781f0da7e
Frnkc35 <frankiec35@nycstudents.net>
2024-02-14 13:58:21 -0500

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,10 +1,23 @@
 
 
-x = 4
+""" x = 4
 y = 4
 
 x = input(x,y)
 if (x == 4 , y == 4):
     print("yes")
 else:
-    print("no")
+    print("no") """
+
+
+
+
+
+x = int(input("Please enter your first number: "))
+y = int(input("Please enter your second number: "))
+
+def gcf(x,y):
+    if x > y:
+        print("testing")
+
+gcf(x,y)
