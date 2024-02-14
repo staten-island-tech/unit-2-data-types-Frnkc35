f81c51066656c085a17f94e2bf9c1c7939478d64
Frnkc35 <frankiec35@nycstudents.net>
2024-02-14 18:53:12 -0500

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -27,12 +27,47 @@ gcf(x,y)
 
 
 
-def two(x):
-    print(f"Your number is {x}")
+""" def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
 
-two(32985732)
+full_name = create_name("frankie", "chen")
+
+print(full_name) """
 
 
+
+""" number = int(input("Enter number to find factors: "))
+
+print("Factors of number {} are : ".format(number))
+for i in range(1, number + 1):
+    if(number % i == 0):
+        print(i) """
+
+
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiply")
+print("4 - Divide")
+
+calculate = int(input("Choose your operation:" ))
+if calculate == 1:
+    x = int(input("Please enter your first number:" ))
+    y = int(input("Please enter your second number:" ))
+    print(x + y)
+elif calculate == 2:
+    x = int(input("Please enter your first number:" ))
+    y = int(input("Please enter your second number:" ))
+    print(x - y)
+elif calculate == 3:
+    x = int(input("Please enter your first number:" ))
+    y = int(input("Please enter your second number:" ))
+    print(x * y)
+elif calculate == 4:
+    x = int(input("Please enter your first number:" ))
+    y = int(input("Please enter your second number:" ))
+    print(x / y)
 
 
 
